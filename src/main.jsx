ce1621e34a80b1e5c3e43adb72848bfdb13f4724
Diego Iturra <diturra@dparadig.com>
2024-02-27 19:17:09 -0300

    add LoginForm component to main for test purposes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import LoginForm from './components/Login/LoginForm.jsx'
 
 //Imports for testing components
 //import FileUploader from './components/FileUploader.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LoginForm/>
   </React.StrictMode>,
 )
