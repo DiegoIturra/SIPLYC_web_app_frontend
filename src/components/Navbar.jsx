46fe822eb38fce37e480a6ea47c0b3c5042a64c0
Diego Iturra <diturra@dparadig.com>
2024-03-01 17:16:53 -0300

    refactor: use new Navbar style only for desing purpose

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,33 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, Link, useNavigate } from "react-router-dom"
 
 export const NavBar = () => {
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    navigate('/login', {
+      replace: true
+    })
+  }
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary pb-3">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">SIPLYC</a>
-          
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <NavLink className={ ({isActive}) =>  `nav-item nav-link ${ isActive ? 'active' : ''}`} to="/home">Home</NavLink>
-              <NavLink className={ ({isActive}) =>  `nav-item nav-link ${ isActive ? 'active' : ''}`} to="/children">Niños</NavLink>
-              <NavLink className={ ({isActive}) =>  `nav-item nav-link ${ isActive ? 'active' : ''}`} to="/teachers">Tutores</NavLink>
-              <NavLink className={ ({isActive}) =>  `nav-item nav-link ${ isActive ? 'active' : ''}`} to="/kinderGardens">Jardín</NavLink>
-              <NavLink className={ ({isActive}) =>  `nav-item nav-link ${ isActive ? 'active' : ''}`} to="/home">Nomina</NavLink>
-            </div>
-          </div>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+        <Link className="navbar-brand" to="/home">SIPLYC</Link>
 
+        <div className="container-fluid">
+          <div className="navbar-nav">
+            <NavLink className={ ({isActive}) =>  `nav-item nav-link ${ isActive ? 'active' : ''}`} to="/children">Niños</NavLink>
+            <NavLink className={ ({isActive}) =>  `nav-item nav-link ${ isActive ? 'active' : ''}`} to="/kinderGardens">Jardines</NavLink>
+            <NavLink className={ ({isActive}) =>  `nav-item nav-link ${ isActive ? 'active' : ''}`} to="/teachers">Tutores</NavLink>
+          </div>
+        </div>
+
+        <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+          <ul className="navbar-nav ml-auto">
+            <span className='nav-item nav-link text-primary'>currentUser</span>
+            <button className='nav-item nav-link btn' onClick={onLogout}>Logout</button>
+          </ul>
         </div>
       </nav>
     </>
