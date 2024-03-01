28b462e01dfac7facfe305ac2c8907d8971e712b
Diego Iturra <diturra@dparadig.com>
2024-03-01 16:47:28 -0300

    refactor: create a new folder called components inside auth folder for login purposes

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { SiplycApp } from './SiplycApp'
 import { BrowserRouter } from 'react-router-dom'
 
-//Imports for testing components
-//import FileUploader from './components/FileUploader.jsx'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
