431a5ff0a0c5c5f30252e1e058e5c91f7daad5d9
Diego Iturra <diturra@dparadig.com>
2024-03-09 22:45:50 -0300

    refactor: use icons for edit and delete button in Item component

--- src/siplyc/components/Item.jsx
+++ src/siplyc/components/Item.jsx
@@ -1,11 +1,21 @@
+import { BsPencilFill } from "react-icons/bs";
+import { FaTrashAlt } from "react-icons/fa";
+
 export const Item = ({ item }) => {
   return (
     <li className="list-group-item d-flex justify-content-between">
       <span className="align-self-center">{item}</span>
 
       <div className="d-flex justify-content-evenly">
-        <button className="btn btn-warning me-2">Editar</button>
-        <button className="btn btn-danger ms-2">Borrar</button>
+      
+        <button className="btn btn-warning me-2">
+          <BsPencilFill />
+        </button>
+        
+        <button className="btn btn-danger ms-2">
+          <FaTrashAlt />
+        </button>
+      
       </div>
     </li>
   );
