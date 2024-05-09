65fd99a8ed14d0b9d1e8d202f4694e5e7e99db8b
Diego Iturra <diturra@dparadig.com>
2024-05-08 22:34:25 -0400

    fix: delete defaultValue property

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,8 +1,8 @@
-export const Dropdown = ({data, title, name, value, defaultValue, onChange, showDefaultOption}) => {
+export const Dropdown = ({data, title, name, value, onChange, showDefaultOption}) => {  
   return (
     <div className="form-group">
       <label htmlFor="dropdown-input">{title}</label>
-      <select name={name} onChange={onChange} value={value} defaultValue={defaultValue} className="form-control" id="dropdown-input">
+      <select name={name} onChange={onChange} value={value} className="form-control" id="dropdown-input">
         {showDefaultOption && <option value="">Selecciona un {title}</option>}
         {
           data.map((element) => (
