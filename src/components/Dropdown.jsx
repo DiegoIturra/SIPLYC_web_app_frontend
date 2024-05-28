f60703640c117720a887f469a2b1d21344ec88bf
Diego Iturra <diturra@dparadig.com>
2024-05-27 22:58:47 -0400

    fix: Correct use of name and keyName

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,8 +1,8 @@
-export const Dropdown = ({data, title, keyName = 'name', value, onChange, showDefaultOption}) => {  
+export const Dropdown = ({data, title, name, keyName = 'name', value, onChange, showDefaultOption}) => {  
   return (
     <div className="form-group">
       <label htmlFor="dropdown-input">{title}</label>
-      <select name={keyName} onChange={onChange} value={value} className="form-control" id="dropdown-input">
+      <select name={name} onChange={onChange} value={value} className="form-control" id="dropdown-input">
         {showDefaultOption && <option value="">Selecciona un {title}</option>}
         {
           data.map((element) => (
